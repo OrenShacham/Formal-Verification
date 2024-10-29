e0c0baf70d6ad153e101f720a1004732eb8d91df
OrenShacham <orenshacham@mail.tau.ac.il>
2024-10-29 19:22:46 +0200

    updated test
    got rid of unused code, changed the test a bit

--- parser/Test.py
+++ parser/Test.py
@@ -3,29 +3,14 @@ import parser
 import verifier
 
 
-code = parser.program.parse_or_raise("""
-    if x >= y  then { 
-        y := x + 1 ;
-    } else 
-        skip;
-    end
-    """)[0]
-
 wcode = parser.program.parse_or_raise("""
     z := 5;
     y := 5;
-    x := 40;
     while x > y do{
         z := z + 5;
         y := y + 5;
         x := x - 10;
     }end
     """)
-# post_condition = verifier.parse_as_condition("(1>1) && (((1>1 && 2>2) && c>c) && ((1>1 && 2>2) && 3>3))")
 k = verifier.verify_code(wcode, "z = y")
 print(k)
-"""
-verifier.calculate_wlp(condition, code)
-print(condition)
-verifier.test_condition(condition)
-"""
